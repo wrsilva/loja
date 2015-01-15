00fa85fb4db882b2ccc552bfbdc2677719238f05
wrsilva <wr.silva91@gmail.com>
2015-01-14 22:12:32 -0300

    Initial commit

---
+++ wr.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace wr.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ wr.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wr.LojaVirtual.Web.MvcApplication" Language="C#" %>
