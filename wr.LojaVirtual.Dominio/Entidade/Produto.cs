8e00c4e935d95ac9a21287f4d7bc05cec2f73e9e
wrsilva <wr.silva91@gmail.com>
2015-01-15 19:49:34 -0300

    Classe Produto

---
+++ wr.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace wr.LojaVirtual.Dominio.Entidade
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string NomeProduto { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+
+
+    }
+}
